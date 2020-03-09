131fca4e5ec4df89ba36ae753b277843da0cebd1
RCWei <rcwei44@qq.com>
2020-03-09 14:12:58 +0800

    Add JavaScript

---
+++ AllSiteDefaultFont.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         所有站点使用默认字体样式
+// @namespace    https://rickweii.github.io
+// @version      0.1
+// @description  设置所有网站字体样式：微软雅黑+取消加粗
+// @author       RCWei
+// @include        *:*
+// @grant        none
+// @require      https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js
+// ==/UserScript==
+
+(function() {
+    var e = document.createElement('style');
+    e.innerHTML += '* {font-family:微软雅黑}';
+    e.innerHTML += '* {font-weight:400 !important}';
+    document.documentElement.appendChild(e);
+})();
