81fd0f8ba069254a6e02b4e1db71e654f884a994
RCWei <rcwei44@qq.com>
2020-03-15 08:58:24 +0800

    新增 exclude meta

--- AllSiteDefaultFont.user.js
+++ AllSiteDefaultFont.user.js
@@ -1,10 +1,13 @@
 // ==UserScript==
 // @name         所有站点使用默认字体样式
 // @namespace    https://github.com/rickweii/AllSiteDefaultFont
-// @version      0.1
+// @version      0.12
 // @description  设置所有网站字体样式：微软雅黑+取消加粗
 // @author       RCWei
 // @include        *:*
+// @exclude        *://bigjpg.com/*
+// @exclude        *://*.carrotchou.blog/*
+// @exclude        *://sticker.weixin.qq.com/*
 // @grant        none
 // @require      https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js
 // ==/UserScript==
