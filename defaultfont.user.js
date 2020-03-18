3bc4c4de2ae91982add61d3ff77580afe03930d7
Rick Wei Hsiao <41941069+weirick@users.noreply.github.com>
2020-03-18 17:13:40 +0800

    DefaultFont.user.js

---
+++ defaultfont.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name              所有站点使用默认字体样式
+// @namespace         https://gitub.com/weirick/AllSiteDefaultFont
+// @version           0.44
+// @icon
+// @description       设置所有网站字体样式：微软雅黑+取消加粗
+// @author            RCWei
+// @license           GPL-3.0
+// @supportURL        https://gitub.com/weirick/AllSiteDefaultFont
+// @updateURL         https://gitub.com/weirick/AllSiteDefaultFont/defaultfont.user.js
+// @downloadURL       https://gitub.com/weirick/AllSiteDefaultFont/defaultfont.user.js
+// @include             *://*/*
+// @noframes
+// @run-at            document-start
+// @grant             none
+// ==/UserScript==
+
+;(function () {
+  let css = document.createElement('style'); 
+  css.type = 'text/css'; 
+  css.innerHTML="* {font-family:微软雅黑; font-weight:400 !important;}"; 
+  document.getElementsByTagName('head').item(0).appendChild(css);
+})()
