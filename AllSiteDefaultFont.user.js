11ef009aee01c2799838459783df61e617190ae6
Rick Wei Hsiao <41941069+weirick@users.noreply.github.com>
2020-03-18 17:11:47 +0800

    Update AllSiteDefaultFont.user.js

--- AllSiteDefaultFont.user.js
+++ AllSiteDefaultFont.user.js
@@ -1,20 +1,23 @@
 // ==UserScript==
-// @name         所有站点使用默认字体样式
-// @namespace    https://tampermonkey.net
-// @version      0.20
-// @description  设置所有网站字体样式：微软雅黑+取消加粗
-// @author       RCWei
-// @include        *:*
-// @exclude        *://bigjpg.com/*
-// @exclude        *://*.carrotchou.blog/*
-// @exclude        *://sticker.weixin.qq.com/*
-// @grant        none
-// @require      https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js
+// @name              所有站点使用默认字体样式
+// @namespace         https://gitub.com/weirick/AllSiteDefaultFont
+// @version           0.44
+// @icon
+// @description       设置所有网站字体样式：微软雅黑+取消加粗
+// @author            RCWei
+// @license           GPL-3.0
+// @supportURL        https://gitub.com/weirick/AllSiteDefaultFont
+// @updateURL         https://gitub.com/weirick/AllSiteDefaultFont/defaultfont.user.js
+// @downloadURL       https://gitub.com/weirick/AllSiteDefaultFont/defaultfont.user.js
+// @include             *://*/*
+// @noframes
+// @run-at            document-start
+// @grant             none
 // ==/UserScript==
 
-(function() {
-    var e = document.createElement('style');
-    e.innerHTML += '* {font-family:微软雅黑}';
-    e.innerHTML += '* {font-weight:400 !important}';
-    document.documentElement.appendChild(e);
-})();
+;(function () {
+  let css = document.createElement('style'); 
+  css.type = 'text/css'; 
+  css.innerHTML="* {font-family:微软雅黑; font-weight:400 !important;}"; 
+  document.getElementsByTagName('head').item(0).appendChild(css);
+})()
