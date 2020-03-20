703348d58bdd3b7c2b01a3e4a0e56d077f2757b0
RCWei <rcwei44@qq.com>
2020-03-20 08:31:53 +0800

    SourceHanSanNormal.user.js

---
+++ SourceHanSanNormal.user.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name              所有站点使用默认字体样式(思源黑体)
+// @namespace         https://gitub.com/weirick/AllSiteDefaultFont
+// @version           1.0.3b
+// @icon              https://weirick.github.io/storage/font.png
+// @description       设置所有网站字体样式：思源黑体+取消加粗
+// @author            RCWei
+// @license           GPL-3.0
+// @supportURL        https://gitub.com/weirick/AllSiteDefaultFont
+// @include             *://*/*
+// @noframes
+// @run-at            document-end
+// @grant             none
+// @require           https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js
+// ==/UserScript==
+
+;(function () {
+  let css = document.createElement('style');
+  css.type = 'text/css';
+  css.innerHTML="*{font-family:思源黑体;font-weight:350!important;}a,h1,h2,h3,h4,h5,h6,p,b{font-weight:350!important;}.h1,.h2,.h3,.h4,.h5,.h6,{font-weight:350!important;}";
+  document.getElementsByTagName('head').item(0).appendChild(css);
+  $('pre,code').css('cssText', 'font-weight:350 !important;font-family:Jetbrains Mono !important');
+
+  let body = document.getElementsByTagName('body')[0];
+  body.setAttribute('style','font-weight:350 !important');
+
+  var site = location.host;
+  switch (site) {
+    case 'github.com':
+      $('.h1, .h2, .h3, .h4, .h5, .h6, p, a').css('cssText', 'font-weight:350 !important');
+      $('.text-bold').css('cssText', 'font-weight:350 !important');
+      break;
+
+    case 'member.bilibili.com':
+      $('#app *').css('cssText', 'font-weight:350 !important');
+      break;
+
+    default:
+      $('*').css('cssText', 'font-weight:350 !important');
+  }
+
+})()
